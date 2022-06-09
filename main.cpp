701e7d1f188a04eabb1ce9c4a16b1268e9fb3638
danil <natalya.haritonov.72@gmail.com>
2022-06-09 14:47:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 
-
 using namespace std;
+
+class Klitina
+{
+private:
+    /* data */
+public:
+    
+Klitina(/* args */);
+    ~
+Klitina();
+};
+
+Klitina::Klitina(/* args */)
+{
+}
+
+Klitina::~Klitina()
+{
+}
+
 
 int main()
 {
+
     return 0;
 }
